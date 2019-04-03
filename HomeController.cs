a46e85cfb56eba97a2ea3ba9ddbbe110a7daeb1f
Gabriel <49046521+Eifhen@users.noreply.github.com>
2019-04-03 14:25:18 -0300

    Create HomeController.cs

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalCodeFirst.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Home()
+		{
+
+			return View();
+		}
+		public ActionResult ModuloMantenimiento ()
+		{
+
+			return View();
+		}
+		public ActionResult ModuloProcesos()
+		{
+			return View();
+		}
+
+		public ActionResult ModuloInforme()
+		{
+			return View();
+		}
+
+
+	}
+}
