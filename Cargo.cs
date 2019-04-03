9cf66b50c2f2d9a6bf9af1d0ecb0918a813a2a27
Gabriel <49046521+Eifhen@users.noreply.github.com>
2019-04-03 19:42:48 -0300

    Create Cargo.cs

---
+++ Cargo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalCodeFirst.Models
+{
+	public class Cargo
+	{
+		public int id { get; set; }
+		public String NombreCargo { get; set; }
+	}
+}
