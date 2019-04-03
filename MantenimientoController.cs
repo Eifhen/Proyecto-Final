befedff94a071dfd66a3cb302b5d68e96cf90a18
Gabriel <49046521+Eifhen@users.noreply.github.com>
2019-04-03 19:40:44 -0300

    Create MantenimientoController.cs

---
+++ MantenimientoController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalCodeFirst.Models;
+
+namespace FinalCodeFirst.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+		// GET: Mantenimiento
+
+		Contexto BD = new Contexto();
+
+
+		// *****************************************************************
+		//                   Modulo de Mantenimiento
+		// ******************************************************************
+
+
+
+		[HttpGet]
+		public ActionResult Departamento()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Departamento(Departamento dep)
+		{
+
+			BD.Departamentos.Add(dep);
+			BD.SaveChanges();
+			return RedirectToAction("TablaDepartamento");		
+
+		}
+
+		public ActionResult TablaDepartamento()
+		{
+			return View(BD.Departamentos.ToList());
+		}
+
+		[HttpGet]
+		public ActionResult Cargo()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Cargo(Cargo c)
+		{
+
+			BD.Cargos.Add(c);
+			BD.SaveChanges();
+			return RedirectToAction("TablaCargos");
+		}
+
+		public ActionResult TablaCargos()
+		{
+			return View(BD.Cargos.ToList());
+		}
+	}
+}
